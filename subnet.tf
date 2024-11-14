af0a7675994d53711a7c396fdeb7c820e7260592
ilhangursoy1 <ilhangursoy@hotmail.co.uk>
2024-11-14 16:23:39 +0000

    subnet1

---
+++ subnet.tf
@@ -0,0 +1,20 @@
+# Create two subnets
+resource "aws_subnet" "subnet_1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "subnet_1"
+  }
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "subnet_2"
+  }
+}
