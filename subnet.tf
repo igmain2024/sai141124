07a811c027c1b1b2b641a4191c7a8a4f834abde4
ilhangursoy1 <ilhangursoy@hotmail.co.uk>
2024-11-14 16:29:11 +0000

    subnet1

--- subnet.tf
+++ subnet.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "subnet_1" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = data.aws_availability_zones.available.names[0]
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "subnet_1"
@@ -12,7 +12,7 @@ resource "aws_subnet" "subnet_1" {
 resource "aws_subnet" "subnet_2" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = data.aws_availability_zones.available.names[1]
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "subnet_2"
